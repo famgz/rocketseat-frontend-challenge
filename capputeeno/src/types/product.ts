2e0e84fae81f347cd0135ccb832a7d650e65664b
famgz <famgz@proton.me>
2023-10-30 21:02:20 -0300

    feat: add product page

--- capputeeno/src/types/product.ts
+++ capputeeno/src/types/product.ts
@@ -3,5 +3,12 @@ export interface Product {
     name: string,
     price_in_cents: number,
     image_url: string,
-    category: string
+    description?: string,
+    category?: string
+}
+
+export interface ProductFetchResponse {
+    data: {
+        Product: Product
+    }
 }
